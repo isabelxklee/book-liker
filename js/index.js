219c6cb0841cbe7674f75df3a016d3b3d7232c3d
isabel k. lee <hello@isabelklee.co>
2020-06-29 11:40:28 -0400

    render all books in a list

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
-// document.addEventListener("DOMContentLoaded", function() {})
-
 const booksURL = `http://localhost:3000/books`
+const bookList = document.querySelector("#list")
+const bookInfo = document.querySelector("#show-panel")
 
 fetch(booksURL)
 .then(r => r.json())
-.then(console.log)
+.then((books) => {
+  books.forEach((book) => {
+    renderBookList(book)
+  })
+})
 
-function renderBookList() {
-  
+function renderBookList(book) {
+  let title = book.title
+  let description = book.description 
+  let cover = book.img_url
+  let users = book.users
+
+  let listItem = document.createElement("li")
+  listItem.innerText = title
+  bookList.append(listItem)
+
+  listItem.addEventListener("click", (event) => {
+    
+  })
 }
