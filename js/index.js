06ae325fe752d49c160a6008bfc29f40c3fb1028
isabel k. lee <hello@isabelklee.co>
2020-06-29 11:33:35 -0400

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {})
+
+const booksURL = `http://localhost:3000/books`
+
+fetch(booksURL)
+.then(r => r.json())
+.then(console.log)
+
+function renderBookList() {
+  
+}
